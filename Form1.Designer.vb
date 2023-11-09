663a1bbfbeba0f310ceac83b63212d739cfe95ae
Sania <saniaimransyed@gmail.com>
2023-11-09 16:40:26 -0500

    Welcome Page

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,24 +23,36 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Label1 = New Label()
+        Label2 = New Label()
         SuspendLayout()
         ' 
         ' Label1
         ' 
-        Label1.AutoSize = True
-        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
-        Label1.Location = New Point(134, 55)
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(107, 22)
         Label1.Name = "Label1"
-        Label1.Size = New Size(532, 41)
+        Label1.Size = New Size(607, 166)
         Label1.TabIndex = 0
         Label1.Text = "Welcome to Grand Teton National Park"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(347, 281)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Label2"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        BackgroundImage = My.Resources.Resources.clock
+        BackgroundImage = My.Resources.Resources.NewWelcomeImage
         ClientSize = New Size(820, 740)
+        Controls.Add(Label2)
         Controls.Add(Label1)
         Name = "Form1"
         Text = "Form1"
@@ -49,4 +61,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -83,6 +83,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property NewWelcomeImage() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("NewWelcomeImage", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property photo_camera_interface_symbol_for_button() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("photo-camera-interface-symbol-for-button", resourceCulture)
